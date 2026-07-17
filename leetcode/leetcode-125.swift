2bc1c50f5ec46c51881e22c581807f63a940e8e3
Keenan Adityo <keenanadityo167@gmail.com>
2026-07-17 17:25:09 +0700

    leetcode 125

---
+++ leetcode/leetcode-125.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func isPalindrome(_ s: String) -> Bool {
+        var ans = Array(s.lowercased())
+        var l = 0
+        var r = ans.count - 1
+        while l < r {
+            while l < r && !ans[l].isLetter && !ans[l].isNumber {
+                l += 1
+            }
+            while l < r && !ans[r].isLetter && !ans[r].isNumber {
+                r -= 1
+            }
+            if l < r && ans[l] != ans[r] {
+                return false
+            } else {
+                l += 1
+                r -= 1
+            }
+        }
+        return true
+    }
+}
