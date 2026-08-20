cc5d39992bfab4e04b74e4dc1de33e41f5ada5d5
Keenan Adityo <keenanadityo167@gmail.com>
2026-08-20 23:31:38 +0700

    hackerank check palindrom

---
+++ hackerrank/hackerrank-check-palindrome-filter-non-letters.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+/*
+ * Complete the 'isAlphabeticPalindrome' function below.
+ *
+ * The function is expected to return a BOOLEAN.
+ * The function accepts STRING code as parameter.
+ */
+
+bool isAlphabeticPalindrome(string code) {
+
+}
+
+int main()
+{
+    string code;
+    getline(cin, code);
+
+    bool result = isAlphabeticPalindrome(code);
+
+    cout << result << "\n";
+
+    return 0;
+}
