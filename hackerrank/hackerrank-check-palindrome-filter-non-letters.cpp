7289553075dec404843f3ca52819f2e966690ea5
Keenan Adityo <keenanadityo167@gmail.com>
2026-08-21 14:51:08 +0700

    complete hackerrank palindrome problem

--- hackerrank/hackerrank-check-palindrome-filter-non-letters.cpp
+++ hackerrank/hackerrank-check-palindrome-filter-non-letters.cpp
@@ -12,7 +12,26 @@ using namespace std;
  */
 
 bool isAlphabeticPalindrome(string code) {
-
+    string decoded = "";
+    for(int i = 0; i < code.length(); i++) {
+        if(code[i] >= 'a' && code[i] <= 'z') {
+            decoded += code[i];
+        }
+        if(code[i] >= 'A' && code[i] <= 'Z') {
+            decoded += (code[i] + 'a' - 'A');
+        }
+    }
+    int l = 0, r = decoded.length() - 1; 
+    
+    while(l < r) {
+        if(decoded[l] != decoded[r]) {
+            return 0;
+        }
+        l++;
+        r--;
+    }
+    
+    return 1;
 }
 
 int main()
